27ca1f765d26fd362bf1e34fde8ffd3312ba442b
Your Name <you@example.com>
2021-06-24 23:35:35 +0530

    updated day9

--- day9/day9.js
+++ day9/day9.js
@@ -1,1 +1,21 @@
 var seat=document.querySelectorAll('.seats');
+var booked=document.querySelector('#bs').innerHTML;
+var remain=document.querySelector('#rs').innerHTML;
+
+seat.forEach((seats,index)=>{
+    seats.innerHTML="Available";
+    seats.addEventListener('click',()=>{
+            if(seats.classList.contains("after")){
+                seats.classList.remove("after");
+                seats.innerHTML="Available";
+                document.querySelector('#bs').innerHTML=--booked;
+                document.querySelector('#rs').innerHTML=++remain;
+            }
+            else{
+                seats.classList.add("after");
+                document.querySelector('#bs').innerHTML=++booked;
+                document.querySelector('#rs').innerHTML=--remain;
+                seats.innerHTML="Booked";
+            }
+    })
+})
