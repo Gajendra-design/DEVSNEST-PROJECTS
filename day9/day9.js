0f42117e1bb7818df02dcc2c32915a21ea4f8c73
Your Name <you@example.com>
2021-06-25 15:38:37 +0530

    theme feture

--- day9/day9.js
+++ day9/day9.js
@@ -1,7 +1,8 @@
 var seat=document.querySelectorAll('.seats');
 var booked=document.querySelector('#bs').innerHTML;
 var remain=document.querySelector('#rs').innerHTML;
-
+var theme=document.querySelector('.theme');
+var body=document.querySelector('.body');
 seat.forEach((seats,index)=>{
     seats.innerHTML="Available";
     seats.addEventListener('click',()=>{
@@ -18,4 +19,23 @@ seat.forEach((seats,index)=>{
                 seats.innerHTML="Booked";
             }
     })
+})
+theme.addEventListener('click',()=>{
+    let i=0;
+    if(seat[0].classList.contains("seats2")){
+        while(i<36){
+            seat[i].classList.remove("seats2");
+            i++;
+        }
+        body.classList.remove("body1");  
+    }
+    else{
+        while(i<36){
+            seat[i].classList.add("seats2");
+            i++;
+        }
+        body.classList.add("body1");
+        
+    }
+  
 })
