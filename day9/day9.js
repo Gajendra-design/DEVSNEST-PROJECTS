a19296d1b36db28a8a7f8758d536184adb8e8287
Your Name <you@example.com>
2021-06-25 23:20:53 +0530

    dual theme complete

--- day9/day9.js
+++ day9/day9.js
@@ -1,41 +1,49 @@
-var seat=document.querySelectorAll('.seats');
-var booked=document.querySelector('#bs').innerHTML;
-var remain=document.querySelector('#rs').innerHTML;
-var theme=document.querySelector('.theme');
-var body=document.querySelector('.body');
-seat.forEach((seats,index)=>{
-    seats.innerHTML="Available";
-    seats.addEventListener('click',()=>{
-            if(seats.classList.contains("after")){
-                seats.classList.remove("after");
-                seats.innerHTML="Available";
-                document.querySelector('#bs').innerHTML=--booked;
-                document.querySelector('#rs').innerHTML=++remain;
+let totalseats=document.querySelectorAll('.seat');
+let bookedseats=document.querySelector('#bs').innerHTML;
+let remainingseats=document.querySelector('#rs').innerHTML;
+let toggle=document.querySelector('.theme');
+let body=document.querySelector('.body');
+let check=true;
+totalseats.forEach((seat,index)=>{
+    seat.innerHTML="Available";
+    seat.addEventListener('click',()=>{
+        if(seat.classList.contains("afterclicked1")||seat.classList.contains("afterclicked2")){
+            if(check){
+                seat.classList.toggle("afterclicked1");
             }
             else{
-                seats.classList.add("after");
-                document.querySelector('#bs').innerHTML=++booked;
-                document.querySelector('#rs').innerHTML=--remain;
-                seats.innerHTML="Booked";
+                seat.classList.toggle("afterclicked2");
+            }
+               
+                seat.innerHTML="Available";
+                document.querySelector('#bs').innerHTML=--bookedseats;
+                document.querySelector('#rs').innerHTML=++remainingseats;
+            }
+            else{
+                if(check){
+                    seat.classList.toggle("afterclicked1");
+                }
+                else{
+                    seat.classList.toggle("afterclicked2");
+                }
+                document.querySelector('#bs').innerHTML=++bookedseats;
+                document.querySelector('#rs').innerHTML=--remainingseats;
+                seat.innerHTML="Booked";
             }
     })
 })
-theme.addEventListener('click',()=>{
-    let i=0;
-    if(seat[0].classList.contains("seats2")){
-        while(i<36){
-            seat[i].classList.remove("seats2");
-            i++;
+toggle.addEventListener('click',()=>{
+    check=!check;
+    for(let i=0;i<36;i++){
+        totalseats[i].classList.toggle("seat");
+        totalseats[i].classList.toggle("seataftertoggle");
+        if(totalseats[i].classList.contains("afterclicked1")||totalseats[i].classList.contains("afterclicked2")){
+            totalseats[i].classList.toggle("afterclicked1");
+            totalseats[i].classList.toggle("afterclicked2");
         }
-        body.classList.remove("body1");  
     }
-    else{
-        while(i<36){
-            seat[i].classList.add("seats2");
-            i++;
-        }
-        body.classList.add("body1");
-        
-    }
-  
+    body.classList.toggle("body");
+    body.classList.toggle("bodyaftertoggle");
+
+    
 })
