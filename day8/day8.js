0fd5d74f0400087b0d9b59795a398c303b9ac947
Your Name <you@example.com>
2021-07-13 17:11:03 +0530

    day 8 complete

---
+++ day8/day8.js
@@ -0,0 +1,12 @@
+const outerdiv=document.querySelector('.container');
+const template=`<div class="inner"></div>`;
+for(let i=0;i<1600;i++){
+    outerdiv.innerHTML+=template;
+}
+const innerdiv=document.querySelectorAll('.inner');
+console.log(innerdiv);
+innerdiv.forEach((inner)=>{
+inner.addEventListener('click',()=>{
+    inner.classList.toggle('click')
+})
+})
