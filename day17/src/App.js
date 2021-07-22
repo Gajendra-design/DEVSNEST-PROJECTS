17c209753335d0338678091b2ae3c9d16419445c
Your Name <you@example.com>
2021-07-23 00:33:38 +0530

    day 17 complete

--- day17/src/App.js
+++ day17/src/App.js
@@ -1,36 +1,35 @@
 import './App.css';
-import styled from 'styled-components';
-import Card from './components/Card';
+
 
 function App() {
 
   return (
-    <div className="app">
-        <h3>Calorie Read Only</h3>
-        <Container>
-            <Card title="Pizza" calorie="56"/>
-            <Card title="Burger" calorie="69"/>
-            <Card title="Coke" calorie=" 500"/>
-            <Card title="Browne" calorie="180"/>
-            <Card title="Fried Rice" calorie="90"/>
-            <Card title="Lassania" calorie="200"/>
-            <Card title="Pani Puri" calorie="10"/>
-        </Container>
-    </div>
+    <div className="container">
+  <h1>Read Only Calory Container</h1> 
+  <div className="inner">
+  <Card title="burger" calory="150"/>
+  <Card title="pizza" calory="250"/>
+  <Card title="panipuri" calory="350"/>
+  <Card title="rice" calory="50"/>
+  <Card title="chapati" calory="450"/>
+  <Card title="chaumin" calory="650"/>
+  <Card title="ice-crim" calory="550"/>
+  <Card title="noodals" calory="450"/>
+  <Card title="kachori" calory="10"/>
+  <Card title="pasta" calory="15"/>
+  <Card title="samosa" calory="750"/>
+  <Card title="pastri" calory="850"/>
+  </div>
+  </div>
   );
-}
-
-const Container=styled.div`
-width:50vw;
-height:70vh;
-min-height:60vh;
-border-radius:0.2rem;
-overflow-y:auto;
-overflow-x:hidden;
-box-shadow:0px 3px 5px rgba(0,0,0);
-padding-top:1rem;
-padding-left:0.5rem;
-padding-right:0.5rem;
-padding-top:1rem;
-`
-export default App;
+  }
+  const Card=({title,calory})=>{
+    return(
+    <div className="info">
+        <h2>{title}</h2>
+        <p>you will gained {calory} from this food</p>
+    </div>
+    
+    )
+  }
+  export default App;
